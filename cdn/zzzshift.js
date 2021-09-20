5997fd3c8aee84312cede59ca557577cf00a211f
Get-DevVed-YT <74463375+Get-DevVed-YT@users.noreply.github.com>
2021-09-19 22:28:32 -0400

    CDN unminified

---
+++ cdn/zzzshift.js
@@ -0,0 +1,29 @@
+function zzzEncode(text){
+  let regex = /[a-zA-Z0-9_@./#$&+-?!,]/;
+  let letters = text.split("");
+  let newText = text.replace(/[a-zA-Z0-9_@./#$&+-?!,]/g, "z");
+  let keyArray = [];
+  letters.forEach(function(letter) 
+  {
+    if (regex.test(letter)){
+       let letterVal = "z".charCodeAt(0) - letter.charCodeAt(0);
+       keyArray.push(letterVal);
+    } else {
+       keyArray.push(0);
+    }
+  });
+  let key = keyArray.join(".");
+  return [newText, key];
+}
+
+function zzzDecode(text, key){
+  let keyArray = key.split(".");
+  let letters = text.split("");
+  let textArray = [];
+ letters.forEach(function(letter, i){
+   let value = parseInt(letter.charCodeAt(0)) - parseInt(keyArray[i]);
+textArray.push(value)
+ })
+  let string = String.fromCharCode(...textArray);
+  return string;
+}
